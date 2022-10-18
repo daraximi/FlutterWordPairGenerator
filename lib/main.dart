4e7fe90a6bf85a89bc7eeb11bf7a7f97cd15d1fb
Darasimi Oloyede <darasimioloyede@gmail.com>
2022-10-18 15:03:17 +0100

    Created repository and added files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final myWordPair = WordPair.random();
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.green), home: RandomWords());
+  }
+}
